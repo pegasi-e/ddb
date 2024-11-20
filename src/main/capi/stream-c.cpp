77f9b69b11dd72dcc5789e8d38916bcd303be50f
223104714 <murali.vemulapati@ge.com>
2024-11-20 14:26:51 -0800

    log 5

--- src/main/capi/stream-c.cpp
+++ src/main/capi/stream-c.cpp
@@ -1,7 +1,7 @@
 #include "duckdb/main/capi/capi_internal.hpp"
 #include "duckdb/common/types/timestamp.hpp"
 #include "duckdb/common/allocator.hpp"
-
+#include "duckdb/common/printer.hpp"
 duckdb_data_chunk duckdb_stream_fetch_chunk(duckdb_result result) {
 	if (!result.internal_data) {
 		return nullptr;
@@ -15,20 +15,30 @@ duckdb_data_chunk duckdb_stream_fetch_chunk(duckdb_result result) {
 }
 
 duckdb_data_chunk duckdb_fetch_chunk(duckdb_result result) {
+  duckdb::Printer::PrintF("duckdb_fetch_chunk1\n");
 	if (!result.internal_data) {
+            duckdb::Printer::PrintF("duckdb_fetch_chunk2\n");
 		return nullptr;
 	}
+        duckdb::Printer::PrintF("duckdb_fetch_chunk3\n");
 	auto &result_data = *((duckdb::DuckDBResultData *)result.internal_data);
+          duckdb::Printer::PrintF("duckdb_fetch_chunk4\n");
 	if (result_data.result_set_type == duckdb::CAPIResultSetType::CAPI_RESULT_TYPE_DEPRECATED) {
+            duckdb::Printer::PrintF("duckdb_fetch_chunk5\n");
 		return nullptr;
 	}
+          duckdb::Printer::PrintF("duckdb_fetch_chunk6\n");
 	result_data.result_set_type = duckdb::CAPIResultSetType::CAPI_RESULT_TYPE_STREAMING;
+          duckdb::Printer::PrintF("duckdb_fetch_chunk7\n");
 	auto &result_instance = (duckdb::QueryResult &)*result_data.result;
+          duckdb::Printer::PrintF("duckdb_fetch_chunk8\n");
 	// FetchRaw ? Do we care about flattening them?
 	try {
 		auto chunk = result_instance.Fetch();
+                  duckdb::Printer::PrintF("duckdb_fetch_chunk9\n");
 		return reinterpret_cast<duckdb_data_chunk>(chunk.release());
 	} catch (std::exception &e) {
+            duckdb::Printer::PrintF("duckdb_fetch_chunk10\n");
 		return nullptr;
 	}
 }
