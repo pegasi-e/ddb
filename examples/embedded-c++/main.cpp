7d65bd3bfcdd7004c5966f093e9045e0788ae2ac
223104714 <murali.vemulapati@ge.com>
2024-12-06 18:05:15 -0800

    upsert

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -2,13 +2,25 @@
 
 using namespace duckdb;
 
-int main() {
-	DuckDB db(nullptr);
+int main(int argc, char* argv[]) {
+	DuckDB db(argv[1]);
 
 	Connection con(db);
-
-	con.Query("CREATE TABLE integers(i INTEGER)");
-	con.Query("INSERT INTO integers VALUES (3)");
-	auto result = con.Query("SELECT * FROM integers");
-	result->Print();
+        std::string arg2(argv[2]);
+        if (arg2 == "create")
+          {
+            con.Query("CREATE TABLE integers(i INTEGER, j INTEGER)");
+            con.Query("INSERT INTO integers VALUES (1, 100),(2,200)");
+            con.Query("CREATE UNIQUE INDEX idx ON integers (i)");
+            //con.Query("INSERT INTO integers VALUES (1, 101),(2,201)");
+            //auto result = con.Query("SELECT * FROM integers");
+            //result->Print();
+            //con.Query("CHECKPOINT 'db0'");
+          }
+        else
+          {
+            con.Query("INSERT INTO integers VALUES (1, 101),(2,201)");
+            auto result = con.Query("SELECT * FROM integers");
+            result->Print();
+          }
 }
