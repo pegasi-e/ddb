00e48a34d9cdf44de6667bf227029d7733214072
Jeremy Osterhoudt <jeremy.osterhoudt@gevernova.com>
2025-10-13 11:45:47 -0700

    forward declare storage index

--- src/transaction/cdc_write_state.cpp
+++ src/transaction/cdc_write_state.cpp
@@ -5,6 +5,7 @@
 #include "duckdb/storage/data_table.hpp"
 #include "duckdb/storage/table/column_data.hpp"
 #include "duckdb/storage/table/update_segment.hpp"
+#include "duckdb/storage/storage_index.hpp"
 #include "duckdb/transaction/append_info.hpp"
 #include "duckdb/transaction/delete_info.hpp"
 #include "duckdb/transaction/duck_transaction.hpp"
