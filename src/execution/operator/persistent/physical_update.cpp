bb544cdeb4c256356ce9f89520c44cd1267ece48
Jeremy Osterhoudt <jeremy.osterhoudt@gevernova.com>
2025-10-14 16:52:02 -0700

    remove cdc changes

--- src/execution/operator/persistent/physical_update.cpp
+++ src/execution/operator/persistent/physical_update.cpp
@@ -117,22 +117,22 @@ SinkResultType PhysicalUpdate::Sink(ExecutionContext &context, DataChunk &chunk,
 
 	// start Anybase changes
 	//Extract the involved columns for CDC
-	auto &transaction = DuckTransaction::Get(context.client, table.db);
-	unordered_map<column_t, vector<column_t>> columnMap;
-	vector<column_t> involved_columns;
-
-	if (context.pipeline->GetSource()->type == PhysicalOperatorType::TABLE_SCAN) {
-		auto table_scan = &context.pipeline->GetSource()->Cast<PhysicalTableScan>();
-		involved_columns.reserve(table_scan->column_ids.size());
-		for (idx_t i = 0; i + 1 < table_scan->column_ids.size(); i++) {
-			involved_columns.emplace_back(table_scan->column_ids[i].GetPrimaryIndex());
-		}
-	}
-
-	for (idx_t i = 0; i < columns.size(); i++) {
-		columnMap[columns[i].index] = involved_columns;
-	}
-	transaction.involved_columns[table.GetTableName()] = std::move(columnMap);
+	// auto &transaction = DuckTransaction::Get(context.client, table.db);
+	// unordered_map<column_t, vector<column_t>> columnMap;
+	// vector<column_t> involved_columns;
+	//
+	// if (context.pipeline->GetSource()->type == PhysicalOperatorType::TABLE_SCAN) {
+	// 	auto table_scan = &context.pipeline->GetSource()->Cast<PhysicalTableScan>();
+	// 	involved_columns.reserve(table_scan->column_ids.size());
+	// 	for (idx_t i = 0; i + 1 < table_scan->column_ids.size(); i++) {
+	// 		involved_columns.emplace_back(table_scan->column_ids[i].GetPrimaryIndex());
+	// 	}
+	// }
+	//
+	// for (idx_t i = 0; i < columns.size(); i++) {
+	// 	columnMap[columns[i].index] = involved_columns;
+	// }
+	// transaction.involved_columns[table.GetTableName()] = std::move(columnMap);
 	//End extract the involved columns for CDC
 	// end Anybase changes
 
