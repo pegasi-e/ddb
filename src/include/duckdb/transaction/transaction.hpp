d811a71bd5c584ebb32c059109a200bb6bb1ebf1
Jeremy Osterhoudt <jeremy.osterhoudt@gevernova.com>
2025-10-16 08:30:52 -0700

    tweak

--- src/include/duckdb/transaction/transaction.hpp
+++ src/include/duckdb/transaction/transaction.hpp
@@ -85,7 +85,8 @@ public:
 	void AddInvolvedColumn(const string table_name, vector<idx_t> &&column_indices) {  // NOLINT
 		// because string is shared it will cause a seg fault without this copy
 		// TODO: merge the columns.
-		involved_columns[string(table_name)] = std::move(column_indices);
+		auto s = string(table_name.c_str());
+		involved_columns[std::move(s)] = std::move(column_indices);
 	}
 	// table -> column indexes -> [involved column indexes]
 	unordered_map<string, vector<idx_t>> involved_columns;
